394bf0aefc7cad964d58e0b40292b81cbd65f162
Fernando-A831 <falvarado@csumb.edu>
2026-02-04 18:31:16 -0800

    Getting started on api
    Getting started on working with api. Have the documentation website linked as a comment, tried commenting everything like professor said.

--- app/src/main/java/com/example/project01group03/API/DiscogsData.kt
+++ app/src/main/java/com/example/project01group03/API/DiscogsData.kt
@@ -1,3 +1,20 @@
 package com.example.project01group03.API
+//library that translates JSON into kotlin objects for the app since its kotlin based
+import com.google.gson.annotations.SerializedName
 
-//api stuff here
+//https://www.discogs.com/developers/#page:database,header:database-artist
+//webpage to documentation and how to use their database
+data class DiscogsSearchResponse(
+    @SerializedName("results") val results: List<SearchResult>
+)
+
+//parameters used on discogs documentation,
+//type for artist, title and id for artist
+data class SearchResult(
+    val id: Int,//id of artist
+    val title: String,//name of artist
+    val type: String,// when you search for a name, multiple different results can come up
+    //discogs uses type to filter for album, artist, label etc
+    @SerializedName("cover_image") val coverImage: String,
+    @SerializedName("resource_url") val resourceUrl: String
+)
